b8c1cb0f4ac80b970af6657f0a5690ad8987d2de
Ben Smithgall <ben.smithgall@gmail.com>
2015-02-03 10:07:52 -0500

    set up test scaffolding

---
+++ test/unit/extractors/test_excel_extractor.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestExcelExtractor(unittest.TestCase):
+	def setUp(self):
+		pass
+
+	def test_convert_to_python_types(self):
+		self.assertTrue(True)
+
+if __name__ == '__main__':
+	unittest.main()
